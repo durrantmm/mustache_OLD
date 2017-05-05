69e554ed2214af603915322f6611ec8a6dbf5869
M Durrant <mdurrant@DN0a22f67a.SUNet>
2017-05-05 14:55:13 -0600

    update

--- scripts/split_sam_by_class.py
+++ scripts/split_sam_by_class.py
@@ -32,6 +32,7 @@ def main(genome_sam_r1, genome_sam_r2, insertseq_sam_r1, insertseq_sam_r2, class
         if not (genome_read1.query_name == genome_read2.query_name == class1[0] == class2[0]):
             print("ERROR: The fastq and class files you provided are not ordered properly. Aborting")
             print("CASE:", sample, genome, inseq)
+            print("NAMES:", genome_read1.query_name, genome_read2.query_name, class1[0], class2[0])
             sys.exit()
 
         if genome_read1.is_unmapped and inseq_read2.is_unmapped and genome_read2.is_unmapped and inseq_read1.is_unmapped:
